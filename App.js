f9744b6d27a115fb9dd8e2b805aa402ddc721065
loki <rus_basket_kg@inbox.ru>
2018-12-12 22:20:38 +0600

    Changed app styles

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Header from './src/components/Header';
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Header/>
       </View>
     );
@@ -14,19 +14,6 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    marginTop: 20
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    
   },
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = (props) => {
-    const { textStyle } = styles;
+    const { textStyle, viewStyle } = styles;
 
-    return (<Text style={textStyle}>Albums</Text>);
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    );
 };
 
 const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8'
+    },
     textStyle: {
         fontSize: 20
     }
