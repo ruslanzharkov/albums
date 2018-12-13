1dbd77011899b4d79da16fa8689363ae4fb1cb32
loki <rus_basket_kg@inbox.ru>
2018-12-13 21:15:14 +0600

    Shadow for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,12 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         height: 60,
-        paddingTop: 15
+        paddingTop: 15,
+        shadowColow: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
     },
     textStyle: {
         fontSize: 20
