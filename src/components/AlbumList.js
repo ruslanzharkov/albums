e58522f4536c1609adeb173d2da9ee66125d4370
loki <rus_basket_kg@inbox.ru>
2018-12-13 21:32:56 +0600

    Connected to root component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = (props) => {
+    return (
+        <View>
+            <Text>Music</Text>
+        </View>
+    )
+}
+
+export default AlbumList;
