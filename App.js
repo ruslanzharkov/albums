c53a002025f9e2b6490331c3971bdc2ea8373e3d
loki <rus_basket_kg@inbox.ru>
2018-12-13 21:21:32 +0600

    Added props to header component

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Header/>
+        <Header headerText={'Albums'}/>
       </View>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
 
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Albums</Text>
+            <Text style={textStyle}>{props.headerText}</Text>
         </View>
     );
 };
