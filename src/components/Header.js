b4f282ac3930ceb2886a1efad5a899218aa581ce
loki <rus_basket_kg@inbox.ru>
2018-12-13 20:59:27 +0600

    Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,11 @@ const Header = (props) => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8'
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15
     },
     textStyle: {
         fontSize: 20
