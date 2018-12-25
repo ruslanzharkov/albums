e6911606b093653b679bf7835e1d2b85838fafa7
loki <rus_basket_kg@inbox.ru>
2018-12-25 20:42:38 +0600

    Added index.js to reducers and to store

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import PostsContainer from './containers/PostContainer';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 
-class HomeScreen extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-      </View>
+        <Provider store={store}>
+
+        </Provider>
     );
-  } // make this for correct works!
-}
+  } 
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: PostsContainer
-  }
-});
-
-export default createAppContainer(AppNavigator);
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+import reducers from '../reducers';
+
+const configureStore = (initialState) => { 
+    return createStore(
+        reducers, initialState
+    );
+};
+
+const store = configureStore({});
+
+export default store;
