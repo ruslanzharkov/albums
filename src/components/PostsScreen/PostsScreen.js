eeb76f16dfe64e9182b2593f5fec9fef0f571273
loki <rus_basket_kg@inbox.ru>
2018-12-29 21:20:12 +0600

    Removed some components

--- src/components/PostsScreen/PostsScreen.js
+++ src/components/PostsScreen/PostsScreen.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import AlbumList from '../AlbumList';
+import {Text, View} from 'react-native';
 
 class PostsScreen extends Component {
-  static navigationOptions = { 
-    title: 'Home'
-  }
-
   render() {
     return (
       <View>
-        <AlbumList />
+        <Text>List</Text>
       </View>
     );
   }
