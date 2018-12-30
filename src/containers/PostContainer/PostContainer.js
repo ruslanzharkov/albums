e019b9b0ccac1acca437b12ad81230377ac61960
loki <rus_basket_kg@inbox.ru>
2018-12-30 14:15:14 +0600

    Removed view component

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import PostsScreen from '../../components/PostsScreen/PostsScreen';
 import {actionCreators} from '../../actions';
 
 class PostContainer extends Component {
     render() {
         return (
-            <View>
-                <PostsScreen
-                    getPosts={this.props.getPosts}
-                    posts={this.props.posts}
-                />
-            </View>
+            <PostsScreen
+                getPosts={this.props.getPosts}
+                posts={this.props.posts}
+            />
         );
     }
 }
