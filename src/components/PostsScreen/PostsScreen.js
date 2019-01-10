835e5168872530c3cd5212d71426337d99789c3b
loki <rus_basket_kg@inbox.ru>
2019-01-11 01:36:30 +0600

    Added header for example view in iphone x

--- src/components/PostsScreen/PostsScreen.js
+++ src/components/PostsScreen/PostsScreen.js
@@ -7,8 +7,16 @@ class PostsScreen extends Component {
     }
 
     render() {
+        const { textStyle, viewStyle } = styles;
         return (
             <View>
+                <View style={viewStyle}>
+                    <Text 
+                        style={textStyle}
+                    >
+                        Albums
+                    </Text>
+                </View>
                 {this.props.posts && this.props.posts.map((item, index) =>
                     <Text key={index}>{item.name}</Text>)
                 }
@@ -20,5 +28,24 @@ class PostsScreen extends Component {
         );
     }
 }
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColow: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        paddingTop: 15,
+        fontSize: 20
+    }
+};
 
 export default PostsScreen;
