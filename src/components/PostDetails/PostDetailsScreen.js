d7f4eb18c0e1ed8c6e0ae7bea0477b141b7422a9
loki <rus_basket_kg@inbox.ru>
2019-02-05 23:35:48 +0600

    Icon for viewed post count and content

--- src/components/PostDetails/PostDetailsScreen.js
+++ src/components/PostDetails/PostDetailsScreen.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class PostDetailsScreen extends Component {
+    renderContent = () => {
+        return (
+            <View style={styles.containerContent}>
+                <Text style={styles.title}>{this.props.postDetails.title}</Text>
+                <Text>{this.props.postDetails.author}</Text>
+                <Text>{this.props.postDetails.content}</Text>
+            </View>
+        );
+
+    };
+
     render() {
         return (
             <View>
-                <Text>{this.props.postDetails.title}</Text>
+                <View style={styles.containerContent}>
+                    <Ionicons name={'ios-eye'} size={20} color={'black'} />
+                </View>
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    title: {
+        fontSize: 26
+    },
+    containerContent: {
+        marginHorizontal: 10
+    }
+};
 
 export default PostDetailsScreen;
