892a7d92a35b332e0dd6ef1cc3501883b824374a
loki <rus_basket_kg@inbox.ru>
2019-02-11 23:35:41 +0600

    Extends for react component

--- src/components/AddPostScreen/AddPostScreen.js
+++ src/components/AddPostScreen/AddPostScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 
-class AddPostScreen {
+class AddPostScreen extends Component{
     constructor(props) {
         super(props);
+
         this.state = {
             title: '',
             content: ''
