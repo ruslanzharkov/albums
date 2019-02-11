c2fc9ac75a19b66bbeeb9d162fb26c840f560ac8
loki <rus_basket_kg@inbox.ru>
2019-02-11 23:32:04 +0600

    Add post screen

---
+++ src/components/AddPostScreen/AddPostScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View} from 'react-native';
+
+class AddPostScreen {
+    constructor(props) {
+        super(props);
+        this.state ={
+
+        }
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+}
+
+export default AddPostScreen;
--- src/containers/PostDetailsContainer/PostDetailsContainer.js
+++ src/containers/PostDetailsContainer/PostDetailsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import PostDetailsScreen from '../../components/PostDetails';
+import PostDetailsScreen from '../../components/PostDetailsScreen';
 
 class PostDetailsContainer extends Component {
     render() {
