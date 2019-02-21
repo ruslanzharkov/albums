c33f61b6ee27d3b0014642c8c4758e51bb1c25fd
loki <rus_basket_kg@inbox.ru>
2019-02-21 22:46:28 +0600

    Refactored and improved work

--- src/components/AddPostScreen/AddPostScreen.js
+++ src/components/AddPostScreen/AddPostScreen.js
@@ -20,22 +20,8 @@ class AddPostScreen extends Component {
         const author = this.state.author;
         const content = this.state.content;
 
-        if (!title) {
-            this.setState({ emptyTitle: true });
+        if (this.emptyChecker(title, author, content))
             return;
-        }
-
-        if (!author) {
-            this.setState({ emptyAuthor: true });
-            return;
-        }
-
-        if (!content || content.length < 50) {
-            this.setState({ emptyContent: true });
-            return;
-        }
-
-
 
         const date = new Date();
         const postNumber = this.props.posts.length + 1;
@@ -44,41 +30,62 @@ class AddPostScreen extends Component {
         this.props.addNewPost({ title, author, content, formattedDate, postNumber });
     };
 
+    emptyChecker = (title, author, content) => {
+
+        if (!title) {
+            this.setState({ emptyTitle: true });
+            return true;
+        }
+
+        if (!author) {
+            this.setState({ emptyAuthor: true });
+            return true;
+        }
+
+        if (!content || content.length < 50) {
+            this.setState({ emptyContent: true });
+            return true;
+        }
+    };
+
     titleChangeHandler = (title) => {
         this.setState({ title, emptyTitle: false });
     };
 
     authorChangeHandler = (author) => {
-        this.setState({ author });
+        this.setState({ author, emptyAuthor: false });
     };
 
     contentChangeHandler = (content) => {
-        this.setState({ content });
+        this.setState({ content, emptyContent: false });
     };
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.titleInputContainer}>
-                    <Text>Post title</Text>
+                    {this.state.title.length !== 0 ? <Text>Post title</Text> : <View style={{height: 17}}/>}
                     <TextInput
                         style={[styles.titleInput, this.state.emptyTitle ? styles.errorTitleInput : null]}
+                        placeholder={this.state.title.length === 0 ? 'Post title' : null}
                         onChangeText={this.titleChangeHandler}
                     />
                 </View>
 
                 <View style={styles.titleInputContainer}>
-                    <Text>Post author</Text>
+                    {this.state.author.length !== 0 ? <Text>Post author</Text> : <View style={{height: 17}}/>}
                     <TextInput
-                        style={[styles.titleInput, this.state.emptyTitle ? styles.errorTitleInput : null]}
+                        style={[styles.titleInput, this.state.emptyAuthor ? styles.errorTitleInput : null]}
+                        placeholder={this.state.author.length === 0 ? 'Post author' : null}
                         onChangeText={this.authorChangeHandler}
                     />
                 </View>
 
                 <View style={styles.titleInputContainer}>
-                    <Text>Content of post</Text>
+                    {this.state.content.length !== 0 ? <Text>Post content</Text> : <View style={{height: 17}}/>}
                     <TextInput
                         style={[styles.contentInput, this.state.emptyContent ? styles.errorTitleInput : null]}
+                        placeholder={this.state.content.length === 0 ? 'Post content' : null}
                         editable={true}
                         maxLength={4000}
                         multiline={true}
