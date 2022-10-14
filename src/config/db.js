c95edcf76e658078ff2cdd1efb5e0dd16d74f888
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2022-10-15 00:04:56 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 import firebase from 'react-native-firebase';
 
 const config = {  
-    apiKey: 'AIzaSyCvXyjifg2ClJZpoLe55VmSzyhWUPsv2hM',
-    authDomain: 'albums-33d4f.firebaseapp.com',
-    databaseURL: 'https://albums-33d4f.firebaseio.com',
-    storageBucket: 'albums-33d4f.appspot.com',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    storageBucket: '',
 };
 
 const app = firebase.initializeApp(config);  
